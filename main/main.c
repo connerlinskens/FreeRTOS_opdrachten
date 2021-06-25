30b06cc04d8aca9f5f3b95b79fe2a9892d466536
Conner Linskens <conner@linskens.nl>
2021-06-25 12:53:11 +0200

    Week 1 Opdr 2

--- main/main.c
+++ main/main.c
@@ -11,21 +11,40 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "nvs_flash.h"
+#include "driver/gpio.h"
  
+#define BLINK_GPIO 13
+
 void hello_task(void *pvParameter)
 {
-    printf("Hello world!\n");
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
+ 
+	while(1)
+	{
+	    printf("Hello world!\n");
+	    vTaskDelay(100 / portTICK_RATE_MS);
+	}
+}
+ 
+void blinky(void *pvParameter)
+{
+ 
+    gpio_pad_select_gpio(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    while(1) {
+        /* Blink off (output low) */
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        /* Blink on (output high) */
+        gpio_set_level(BLINK_GPIO, 1);
         vTaskDelay(1000 / portTICK_RATE_MS);
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
+ 
  
 void app_main()
 {
     nvs_flash_init();
     xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
+    xTaskCreate(&blinky, "blinky", 512,NULL,5,NULL );
 }
