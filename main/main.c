c598b03322545daf9ba93a127564ae9e70d7f873
Conner Linskens <conner@linskens.nl>
2021-06-25 16:25:56 +0200

    Week 3 - 1

--- main/main.c
+++ main/main.c
@@ -5,72 +5,76 @@
 #include "esp_system.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
+#include "esp_spi_flash.h"
+#include "freertos/timers.h"
+#include "freertos/semphr.h"
 
-static const char* task1Text = "Task 1\n";
-static const char* task2Text = "Task 2\n";
-static const char* task3Text = "Task 3\n";
-static const char* task4Text = "Task 4\n";
-static const char* task5Text = "Task 5\n";
-static const char* task6Text = "Task 6\n";
-static const char* task7Text = "Task 7\n";
-static const char* task8Text = "Task 8\n";
-static const char* task9Text = "Task 9\n";
-static const char* task10Text = "Task 10\n";
-static const char* task11Text = "Task 11\n";
-static const char* task12Text = "Task 12\n";
-static const char* task13Text = "Task 13\n";
-static const char* task14Text = "Task 14\n";
-static const char* task15Text = "Task 15\n";
-static const char* task16Text = "Task 16\n";
-static const char* task17Text = "Task 17\n";
-static const char* task18Text = "Task 18\n";
-static const char* task19Text = "Task 19\n";
-static const char* task20Text = "Task 20\n";
+#define NUM_OF_PHILOSOPHERS 5
+#define MAX_NUMBER_ALLOWED (NUM_OF_PHILOSOPHERS - 1)
+#define left(i) (i)
+#define right(i) ((i + 1) % NUM_OF_PHILOSOPHERS)
 
+SemaphoreHandle_t forks[NUM_OF_PHILOSOPHERS];
+SemaphoreHandle_t philosophers_sem;
+TaskHandle_t philosophers[NUM_OF_PHILOSOPHERS];
 
-void task(void *pvParameter)
+void think_task(void *pvParameter);
+
+void take_fork(int i){
+    TaskHandle_t think_task_handle = NULL;
+    xTaskCreate(think_task, "think_task", 2048, &i, 2, &think_task_handle);
+
+    xSemaphoreTake(forks[left(i)], portMAX_DELAY);
+    xSemaphoreTake(forks[right(i)], portMAX_DELAY);
+
+    vTaskDelete(think_task_handle);
+}
+
+void return_fork(int i){
+    xSemaphoreGive(forks[left(i)]);
+    xSemaphoreGive(forks[right(i)]);
+    printf("Philosopher %d puts fork %d and %d back\n", i, left(i), right(i));
+}
+
+void think_task(void *pvParameter)
 {
-    char* taskName;
-    taskName = (char*)pvParameter;
-    
+    int i = *(int *) pvParameter;
+    while(1){
+        printf("Philospher %d is thinking\n", i);
+        vTaskDelay(500);
+    }
+}
+
+void philosophers_task(void *pvParameter){
+    int i = *(int *)pvParameter;
     while(true){
-        printf(taskName);
-        vTaskDelay(1 / portTICK_PERIOD_MS);
+        xSemaphoreTake(philosophers_sem, portMAX_DELAY);
+
+        take_fork(i);
+        printf("Philosopher %d is eating\n", i);
+        vTaskDelay(100 / portTICK_RATE_MS);
+        return_fork(i);
+
+        xSemaphoreGive(philosophers_sem);
+        vTaskDelay(100 / portTICK_RATE_MS);
     }
 }
 
 void app_main()
 {
+    int param[NUM_OF_PHILOSOPHERS];
+
+    for(int i = 0; i < NUM_OF_PHILOSOPHERS; i++){
+        forks[i] = xSemaphoreCreateMutex();
+    }
+
+    philosophers_sem = xSemaphoreCreateCounting(MAX_NUMBER_ALLOWED, MAX_NUMBER_ALLOWED);
+
+    for(int i = 0; i < NUM_OF_PHILOSOPHERS; i++){
+        param[i] = i;
+        xTaskCreate(philosophers_task, "philisophers_task", 2048, &param[i], 2, NULL);
+    }
     nvs_flash_init();
-    // Starting tasks dynamicly (doesnt work)
-    // for (int i = 0; i < 20; i++)
-    // {
-    //     char buf[8];
-    //     char* text = snprintf(buf, 8, "Task %d", i);
-    //     xTaskCreate(&task, text, 2048, (void*)text, 5, NULL);
-    // }
 
-    xTaskCreate(&task, "Task 1", 2048, (void*)task1Text, 5, NULL);
-    xTaskCreate(&task, "Task 2", 2048, (void*)task2Text, 5, NULL);
-    xTaskCreate(&task, "Task 3", 2048, (void*)task3Text, 5, NULL);
-    xTaskCreate(&task, "Task 4", 2048, (void*)task4Text, 5, NULL);
-    xTaskCreate(&task, "Task 5", 2048, (void*)task5Text, 5, NULL);
-    xTaskCreate(&task, "Task 6", 2048, (void*)task6Text, 5, NULL);
-    xTaskCreate(&task, "Task 7", 2048, (void*)task7Text, 5, NULL);
-    xTaskCreate(&task, "Task 8", 2048, (void*)task8Text, 5, NULL);
-    xTaskCreate(&task, "Task 9", 2048, (void*)task9Text, 5, NULL);
-    xTaskCreate(&task, "Task 10", 2048, (void*)task10Text, 5, NULL);
-    xTaskCreate(&task, "Task 11", 2048, (void*)task11Text, 5, NULL);
-    xTaskCreate(&task, "Task 12", 2048, (void*)task12Text, 5, NULL);
-    xTaskCreate(&task, "Task 13", 2048, (void*)task13Text, 5, NULL);
-    xTaskCreate(&task, "Task 14", 2048, (void*)task14Text, 5, NULL);
-    xTaskCreate(&task, "Task 15", 2048, (void*)task15Text, 5, NULL);
-    xTaskCreate(&task, "Task 16", 2048, (void*)task16Text, 5, NULL);
-    xTaskCreate(&task, "Task 17", 2048, (void*)task17Text, 5, NULL);
-    xTaskCreate(&task, "Task 18", 2048, (void*)task18Text, 5, NULL);
-    xTaskCreate(&task, "Task 19", 2048, (void*)task19Text, 5, NULL);
-    xTaskCreate(&task, "Task 20", 2048, (void*)task20Text, 5, NULL);
-
-    
 
 }
